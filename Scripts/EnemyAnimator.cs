01072f60a44e7764bbc80ea7d61561b8a10991b9
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-23 19:00:59 -0700

    refactor to use state change instead

--- Scripts/EnemyAnimator.cs
+++ Scripts/EnemyAnimator.cs
@@ -4,17 +4,17 @@ using System;
 public partial class EnemyAnimator : Node {
 	[Export] public Enemy enemy;
 	private AnimationPlayer animator;
-	// Called when the node enters the scene tree for the first time.
+
 	public override void _Ready() {
 		animator = GetNode<AnimationPlayer>("Animation");
 		enemy.Connect("EnemyShoot", Callable.From(() => animator.Play("shoot")));
+		enemy.Connect("MoveStateChange", Callable.From(() => OnMoveStateChange()));
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) {
-		if (enemy.CurrentState == Enemy.MoveState.Idle) {
-			animator.Stop();			
-		} else if (enemy.CurrentState == Enemy.MoveState.Walking) {
+	public void OnMoveStateChange() {
+		// idle = don't play any animations
+		animator.Stop();
+		if (enemy.CurrentState == Enemy.MoveState.Walking) {
 			animator.Play("walk");
 		} else if (enemy.CurrentState == Enemy.MoveState.Falling) {
 			animator.Play("fall");
