66215247ac5910dbaa686e0aa100c0b8fcd93054
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-25 02:47:00 -0700

    add changing of color and toggling of glow

--- Scripts/EnemyVisual.cs
+++ Scripts/EnemyVisual.cs
@@ -22,4 +22,46 @@ public partial class EnemyVisual : Node {
 			animator.Play("charge");
 		}
 	}
+
+	// This approach too specific to model, next time should use interface
+	public void SetColor(Color color) {
+		// get model components
+		MeshInstance3D head = GetNode<MeshInstance3D>("Head");
+		MeshInstance3D body = GetNode<MeshInstance3D>("Body");
+		MeshInstance3D leftWing = GetNode<MeshInstance3D>("LeftWing");
+		MeshInstance3D rightWing = GetNode<MeshInstance3D>("RightWing");
+
+		// TODO: FIX WHERE CHANGING COLOR CHANGES COLOR OF ALL OBJECTS
+		StandardMaterial3D material = head.Mesh.SurfaceGetMaterial(0) as StandardMaterial3D;
+		StandardMaterial3D newMaterial = new(){ AlbedoColor = color };
+		head.Mesh.SurfaceSetMaterial(0, newMaterial);
+		body.Mesh.SurfaceSetMaterial(0, newMaterial);
+		leftWing.Mesh.SurfaceSetMaterial(0, newMaterial);
+		rightWing.Mesh.SurfaceSetMaterial(0, newMaterial);
+
+
+		// // get material
+		// StandardMaterial3D headMaterial = head.Mesh.SurfaceGetMaterial(0) as StandardMaterial3D;
+		// StandardMaterial3D bodyMaterial = body.Mesh.SurfaceGetMaterial(0) as StandardMaterial3D;
+		// StandardMaterial3D leftWingMaterial = leftWing.Mesh.SurfaceGetMaterial(0) as StandardMaterial3D;
+		// StandardMaterial3D rightWingMaterial = rightWing.Mesh.SurfaceGetMaterial(0) as StandardMaterial3D;
+
+		// // set color
+		// headMaterial.AlbedoColor = color;
+		// bodyMaterial.AlbedoColor = color;
+		// leftWingMaterial.AlbedoColor = color;
+		// rightWingMaterial.AlbedoColor = color;
+	}
+
+	public void ToggleGlow(bool glow) {
+		foreach (var child in GetChildren()) {
+			if (child is not MeshInstance3D meshInstance) return;
+			if (meshInstance.Mesh.SurfaceGetMaterial(0) is not StandardMaterial3D material) return;
+			Color originalColor = material.AlbedoColor;
+			if (glow)
+				material.AlbedoColor = new Color(originalColor.R * 2, originalColor.G * 2, originalColor.B * 2, 1);
+			else 
+				material.AlbedoColor = new Color(originalColor.R / 2, originalColor.G / 2, originalColor.B / 2, 1);
+		}
+	}
 }
