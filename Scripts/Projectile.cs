5f49eacd0ea2055e90d74b676d3404e46a0d2360
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-25 10:33:33 -0700

    add collision detection and lifetime counter

--- Scripts/Projectile.cs
+++ Scripts/Projectile.cs
@@ -2,11 +2,34 @@ using Godot;
 using System;
 
 public partial class Projectile : RigidBody3D {
-	public override void _Ready() {
+	// exported variables
+	[Export] public float Gravity = -0.5f;
+	[Export] public Timer LifespanTimer;
 
+	// instance variables
+	private SphereMesh mesh;
+
+	// signals
+	[Signal] public delegate void CollideWithPlayerEventHandler();
+
+	public override void _Ready() {
+		ContactMonitor = true;
+		MaxContactsReported = 10;
+
+		// connect signals
+		Connect("body_entered", Callable.From((PhysicsBody3D body) => OnCollision(body)));
+		LifespanTimer.Connect("timeout", Callable.From(() => OnLifeSpanTimeout()));
 	}
 
 	public override void _Process(double delta) {
 		
+	}
+	
+	private void OnCollision(PhysicsBody3D body) {
+		GD.Print(body.GetType());
+	}
+
+	private void OnLifeSpanTimeout() {
+		QueueFree();
 	}
 }
