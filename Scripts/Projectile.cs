fb3fbae8c05a139701979b8ad78cdf17898a5788
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-25 12:14:46 -0700

    remove collision detection to be handled by the game manager

--- Scripts/Projectile.cs
+++ Scripts/Projectile.cs
@@ -3,30 +3,18 @@ using System;
 
 public partial class Projectile : RigidBody3D {
 	// exported variables
-	[Export] public float Gravity = -0.5f;
 	[Export] public Timer LifespanTimer;
 
 	// instance variables
-	private SphereMesh mesh;
-
-	// signals
-	[Signal] public delegate void CollideWithPlayerEventHandler();
-
+	public int Damage { get; private set; } = 25;
+	
 	public override void _Ready() {
-		ContactMonitor = true;
-		MaxContactsReported = 10;
-
 		// connect signals
-		Connect("body_entered", Callable.From((PhysicsBody3D body) => OnCollision(body)));
 		LifespanTimer.Connect("timeout", Callable.From(() => OnLifeSpanTimeout()));
 	}
 
-	public override void _Process(double delta) {
-		
-	}
-	
-	private void OnCollision(PhysicsBody3D body) {
-		GD.Print(body.GetType());
+	public void PopBubble() {
+		QueueFree();
 	}
 
 	private void OnLifeSpanTimeout() {
