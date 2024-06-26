b65cd6c8a0f0bfeeb6460512da980574445466ef
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-26 16:07:48 -0700

    start collision logic

--- Scripts/Projectile.cs
+++ Scripts/Projectile.cs
@@ -6,14 +6,28 @@ public partial class Projectile : RigidBody3D {
 	[Export] public Timer LifespanTimer;
 
 	// instance variables
-	public int Damage { get; private set; } = 25;
-	
+	public int Damage { get; set; } = 25;
+
 	public override void _Ready() {
+		ContactMonitor = true;
+		MaxContactsReported = 10;
+
 		// connect signals
 		LifespanTimer.Connect("timeout", Callable.From(() => PopBubble()));
+		Connect("body_entered", Callable.From((PhysicsBody3D body) => OnCollision(body)));
 	}
 
-	public void PopBubble() {
+    public void PopBubble() {
 		QueueFree();
+	}
+
+	private void OnCollision(PhysicsBody3D body) {
+		if (body is RigidBody3D) {
+
+		} else if (body is CharacterBody3D) {
+			if (body is not CharacterBody3D characterBody) return;
+			if (characterBody.Name != "Player") return;
+			PopBubble();
+		}
 	}
 }
