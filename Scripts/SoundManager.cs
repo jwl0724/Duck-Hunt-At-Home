1d18b17ab395787d9176ee0703d818562dc1c97e
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-26 16:07:04 -0700

    create script for sound manager

---
+++ Scripts/SoundManager.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class SoundManager : Node3D {
+	[Export] Player player;
+	public override void _Ready() {
+
+	}
+
+	public override void _Process(double delta) {
+		
+	}
+}
