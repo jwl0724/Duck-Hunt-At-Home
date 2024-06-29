c1a7073c53c678882208963465e9949348183925
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-29 09:13:10 -0700

    move grapple handle to player

--- Scripts/Hitscan.cs
+++ Scripts/Hitscan.cs
@@ -5,9 +5,6 @@ public partial class Hitscan : RayCast3D {
 	// exported variables
 	[Export] public Player Player;
 
-	// static variables
-	private static readonly int grappleSpeed = 2000;
-
 	public override void _Ready() {
 		Player.Connect("PlayerShoot", Callable.From(() => OnPlayerShoot()));
 		Player.GrappleGunModel.Connect("GrappleGunLifted", Callable.From(() => OnGrappleGunLifted()));
@@ -21,7 +18,6 @@ public partial class Hitscan : RayCast3D {
 
 	private void OnGrappleGunLifted() {
 		var body = GetCollider();
-		if (body is StaticBody3D) 
-			Player.Velocity = Player.Position.DirectionTo(GetCollisionPoint()) * grappleSpeed * (float) GetPhysicsProcessDeltaTime();
+		if (body is StaticBody3D) Player.GrapplePoint = GetCollisionPoint();
 	}
 }
