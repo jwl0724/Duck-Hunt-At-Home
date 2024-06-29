04a1c3fdac89a9d835132844ab81294afe6d45d9
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-29 06:35:56 -0700

    finish logic for grapple visuals

--- Scripts/GrappleVisual.cs
+++ Scripts/GrappleVisual.cs
@@ -2,9 +2,27 @@ using Godot;
 using System;
 
 public partial class GrappleVisual : Node3D {
+	// exported variables
+	[Export] public Player Player;
+	[Export] public AnimationPlayer Animator;
+
+	[Signal] public delegate void GrappleGunLiftedEventHandler();
+
 	public override void _Ready() {
+		Visible = false; // hide grapple when first loading in
+		Player.Connect("PlayerGrapple", Callable.From(() => OnPlayerGrapple()));
+		Animator.Connect("animation_finished", Callable.From((StringName name) => OnAnimationFinished(name)));
+		Animator.Play("holster");
 	}
 
-	public override void _Process(double delta) {
+	private void OnPlayerGrapple() {
+		if (!Visible) Visible = true;
+		if (Player.Grappled) {
+			Animator.Play("lift");
+		} else Animator.Play("holster");
+	}
+
+	private void OnAnimationFinished(StringName animationName) {
+		if (animationName == "lift") EmitSignal(SignalName.GrappleGunLifted);
 	}
 }
