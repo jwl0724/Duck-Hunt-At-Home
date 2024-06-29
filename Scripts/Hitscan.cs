42b3066f34bfd812cbfe89ff17515aa5682f2055
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-29 07:51:15 -0700

    finish grapple function

--- Scripts/Hitscan.cs
+++ Scripts/Hitscan.cs
@@ -5,13 +5,23 @@ public partial class Hitscan : RayCast3D {
 	// exported variables
 	[Export] public Player Player;
 
+	// static variables
+	private static readonly int grappleSpeed = 2000;
+
 	public override void _Ready() {
 		Player.Connect("PlayerShoot", Callable.From(() => OnPlayerShoot()));
+		Player.GrappleGunModel.Connect("GrappleGunLifted", Callable.From(() => OnGrappleGunLifted()));
 	}
 
 	private void OnPlayerShoot() {
 		var body = GetCollider();
 		if (body is Projectile projectile) projectile.PopBubble();
 		else if (body is Enemy enemy) enemy.OnShot(Player.Attack);
+	}
+
+	private void OnGrappleGunLifted() {
+		var body = GetCollider();
+		if (body is StaticBody3D) 
+			Player.Velocity = Player.Position.DirectionTo(GetCollisionPoint()) * grappleSpeed * (float) GetPhysicsProcessDeltaTime();
 	}
 }
