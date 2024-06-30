c0ff6df5c21f18f8bcd9e496b069c4520f7db58b
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-30 05:36:48 -0700

    add trail effect

--- Scripts/GunVisual.cs
+++ Scripts/GunVisual.cs
@@ -5,8 +5,8 @@ public partial class GunVisual : Node3D {
 	// exported variables
 	[Export] public Player Player;
 	[Export] public AnimationPlayer Animator;
-
-	// instance variables	
+	[Export] public PackedScene BulletTrailModel;
+	
 	public override void _Ready() {
 		Player.Connect("PlayerShoot", Callable.From(() => OnPlayerShoot()));
 		Player.Connect("PlayerReload", Callable.From(() => OnPlayerReload()));
@@ -21,5 +21,12 @@ public partial class GunVisual : Node3D {
 
 	private void OnPlayerShoot() {
 		Animator.Play("fire");
+		Node3D trailModel = BulletTrailModel.Instantiate<Node3D>();
+		MeshInstance3D barrel = GetNode<MeshInstance3D>("Barrel");
+		(Engine.GetMainLoop() as SceneTree).Root.AddChild(trailModel);
+		trailModel.Position = barrel.GlobalPosition;
+		// trailModel.Rotation = new Vector3(Player.GlobalRotation.X, Player.GlobalRotation.Y, Player.GlobalRotation.Z);
+		Node3D rotationalHelper = Player.GetNode<Node3D>("RotationalHelper");
+		trailModel.Rotation = new Vector3(rotationalHelper.Rotation.X, rotationalHelper.Rotation.Y + Mathf.DegToRad(-90), rotationalHelper.Rotation.Z);
 	}
 }
