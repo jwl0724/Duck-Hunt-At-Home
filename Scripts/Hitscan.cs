892c93543b24abf233e1c21b86f6c6149b0ac812
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-01 15:23:51 -0700

    add new signal for shot landed event

--- Scripts/Hitscan.cs
+++ Scripts/Hitscan.cs
@@ -5,6 +5,9 @@ public partial class Hitscan : RayCast3D {
 	// exported variables
 	[Export] public Player Player;
 
+	// signals
+	[Signal] public delegate void ShotLandedEventHandler();
+
 	public override void _Ready() {
 		Player.Connect("PlayerShoot", Callable.From(() => OnPlayerShoot()));
 		Player.GrappleGunModel.Connect("GrappleGunLifted", Callable.From(() => OnGrappleGunLifted()));
@@ -13,7 +16,10 @@ public partial class Hitscan : RayCast3D {
 	private void OnPlayerShoot() {
 		var body = GetCollider();
 		if (body is Projectile projectile) projectile.PopBubble();
-		else if (body is Enemy enemy) enemy.OnShot(Player.Attack);
+		else if (body is Enemy enemy) {
+			EmitSignal(SignalName.ShotLanded);
+			enemy.OnShot(Player.Attack);
+		}
 	}
 
 	private void OnGrappleGunLifted() {
