814a29158cacd7bd30445ff54701f3d0abfbcdfc
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-01 15:23:39 -0700

    create class to manage sounds

---
+++ Scripts/EnemySoundManager.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public partial class EnemySoundManager : Node3D {
+	// exported variables
+	[Export] Enemy Enemy;
+	[Export] SoundCollection SoundCollection;
+
+	// internal timers
+	private const int QuackCD = 5;
+	private float QuackTimer = 0;
+
+	public override void _Ready() {
+		Enemy.Connect("EnemyDied", Callable.From((int score) => SoundCollection.Play("Death")));
+		Enemy.Connect("EnemyShoot", Callable.From(() => SoundCollection.Play("Shoot")));
+		Enemy.Connect("EnemyDamaged", Callable.From(() => OnDamaged()));
+		Enemy.Connect("MoveStateChange", Callable.From(() => OnMoveStateChange()));
+	}
+
+	public override void _Process(double delta) {
+		if (QuackTimer >= QuackCD) {
+			QuackTimer = 0;
+			if (GD.Randi() % 3 != 2) return; // 1 in 3 to quack
+			SoundCollection.Play($"Ambience{GD.Randi() % 4 + 1}");
+		} else QuackTimer += (float) delta;
+	}
+
+	private void OnDamaged() {
+		if (GD.Randi() % 8 != 5) return; // 1 in 8 chance to play sound
+		SoundCollection.Play("Damaged", overlap: false);
+	}
+
+	private void OnMoveStateChange() {
+		if (Enemy.CurrentState == Enemy.MoveState.Charging) SoundCollection.Play("Charge");
+		else SoundCollection.Stop("Charge");
+	}
+}
--- Scripts/PlayerSoundManager.cs
+++ Scripts/PlayerSoundManager.cs
@@ -1,13 +1,25 @@
 using Godot;
 using System;
 
-public partial class SoundManager : Node3D {
-	[Export] Player player;
-	public override void _Ready() {
+public partial class PlayerSoundManager : Node3D {
+	// exported variables
+	[Export] public Player Player;
+	[Export] public Hitscan HitscanLine;
+	[Export] public SoundCollection SoundCollection;
 
+	public override void _Ready() {
+		// connect signals
+		Player.Connect("PlayerGrapple", Callable.From(() => OnPlayerGrapple()));
+		Player.Connect("PlayerDamaged", Callable.From(() => SoundCollection.Play("Damaged")));
+		Player.Connect("PlayerEmptyMag", Callable.From(() => SoundCollection.Play("EmptyMag")));
+		Player.Connect("PlayerReload", Callable.From(() => SoundCollection.Play("Reload")));
+		Player.Connect("PlayerDied", Callable.From(() => SoundCollection.Play("Dead")));
+		Player.Connect("PlayerShoot", Callable.From(() => SoundCollection.Play("Shoot")));
+		HitscanLine.Connect("ShotLanded", Callable.From(() => SoundCollection.Play("ShotLanded")));
 	}
 
-	public override void _Process(double delta) {
-		
+	private void OnPlayerGrapple() {
+		if (!Player.Grappled) return; // player sets grapple state before emitting signal
+		SoundCollection.Play("GrappleHook");
 	}
 }
