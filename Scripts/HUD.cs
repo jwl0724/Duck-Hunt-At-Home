c5aa46fb955f6df90190dafba31c6104a81f22e3
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-30 19:03:49 -0700

    change to animated reticle with hit markers and dynamic color changing

--- Scripts/HUD.cs
+++ Scripts/HUD.cs
@@ -4,20 +4,36 @@ using System;
 public partial class HUD : Control {
 	// exported variables
 	[Export] public Player Player;
+	[Export] public Hitscan HitscanLine;
 	
 	// HUD elements
-	[Export] public TextureRect Crosshair;
+	[Export] public Control ReticleParent;
 
 	public override void _Ready() {
+		// connect signals
 		Player.Connect("PlayerShoot", Callable.From(() => OnPlayerShoot()));
+		ReticleParent.GetNode<Timer>("HitMarkerDuration").Connect("timeout", 
+			Callable.From(() => ReticleParent.GetNode<Sprite2D>("HitMarker").Visible = false));
 	}
 
 	public override void _Process(double delta) {
-		Crosshair.Scale = Crosshair.Scale.Lerp(new Vector2(1, 1), 0.1f);
+		ProcessReticleColor();
 	}
 
 	// SIGNAL HANDLERS
 	private void OnPlayerShoot() {
-		Crosshair.Scale = Crosshair.Scale.Lerp(Crosshair.Scale * 1.5f, 0.9f);
+		ReticleParent.GetNode<AnimatedSprite2D>("ReticleSprite").Play("shoot");
+		if (HitscanLine.GetCollider() is Enemy) {
+			ReticleParent.GetNode<Sprite2D>("HitMarker").Visible = true;
+			ReticleParent.GetNode<Timer>("HitMarkerDuration").Start();
+		}
+	}
+
+	// HELPER FUNCTIONS
+	private void ProcessReticleColor() {
+		if (HitscanLine.GetCollider() is Enemy)
+			ReticleParent.GetNode<AnimatedSprite2D>("ReticleSprite").SelfModulate = new Color(1, 0, 0, 1); // red
+		else
+			ReticleParent.GetNode<AnimatedSprite2D>("ReticleSprite").SelfModulate = new Color(1, 1, 1, 1); // white
 	}
 }
