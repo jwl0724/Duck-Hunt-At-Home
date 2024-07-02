e5d8153d3182e379c69b11453bf5129f71a2621d
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-02 15:39:54 -0700

    rework to allow for music as well as sound effects

--- Scripts/SoundCollection.cs
+++ Scripts/SoundCollection.cs
@@ -3,25 +3,41 @@ using System.Collections.Generic;
 
 public partial class SoundCollection : Node3D {
 	Dictionary<string, AudioStreamPlayer3D> SFXCollection = new();
+	Dictionary<string, AudioStreamPlayer> MusicCollection = new();
 
 	public override void _Ready() {
 		foreach (var child in GetChildren()) {
-			if (child is not AudioStreamPlayer3D audio) continue;
-			SFXCollection.Add(child.Name, audio);
+			if (child is AudioStreamPlayer3D sfx) SFXCollection.Add(child.Name, sfx);
+			else if (child is AudioStreamPlayer music) MusicCollection.Add(child.Name, music);
 		}
 	}
 
 	public void Play(string name, Vector3 Position = new(), bool overlap = true) {
-        if (SFXCollection.TryGetValue(name, out AudioStreamPlayer3D audio)) {
-            if (!Position.IsZeroApprox()) audio.Position = Position;
-			if (overlap) audio.Play();
-			else if (!audio.Playing) audio.Play();
+		if (MusicCollection.TryGetValue(name, out AudioStreamPlayer music)) {
+			if (!music.Playing) music.Play();
+
+		} else if (SFXCollection.TryGetValue(name, out AudioStreamPlayer3D sfx)) {
+            if (!Position.IsZeroApprox()) sfx.Position = Position;
+			if (overlap) sfx.Play();
+			else if (!sfx.Playing) sfx.Play();
 			
         } else GD.PushError($"{name} was not found in the collection");
     }
 
 	public void Stop(string name) {
-		if (SFXCollection.TryGetValue(name, out AudioStreamPlayer3D audio)) audio.Stop();
+		if (MusicCollection.TryGetValue(name, out AudioStreamPlayer music)) music.Stop();
+		else if (SFXCollection.TryGetValue(name, out AudioStreamPlayer3D sfx)) sfx.Stop();
 		else GD.PushError($"{name} was not found in the collection");
+	}
+
+	public bool IsPlaying(string name) {
+		if (MusicCollection.TryGetValue(name, out AudioStreamPlayer music)) {
+			return music.Playing;
+
+		} else if (SFXCollection.TryGetValue(name, out AudioStreamPlayer3D sfx)) {
+			return sfx.Playing;
+
+		} else GD.PushError($"{name} was not found in the collection");
+		return false;
 	}
 }
