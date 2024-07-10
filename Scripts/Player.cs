4f9228fc36dbfff02c966aae21a5e5da45c3f910
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-09 22:27:16 -0700

    adjust physics calculations

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 // FINISH CITY LEVEL WORLD ENVIRONMENT AND LIGHTING
 // MAKE SPACE LEVEL WITH NEON THEME
 // MAKE MENU SCREEN (NEW SCENE WITH PANNING BACKGROUND)
-// REFACTOR CODE
 // IRON OUT EGREGIOUS BUGS
 // MAKE GAME OVER SCREEN (REGULAR SCREEN)
 // IRON OUT MINOR BUGS
@@ -25,27 +24,25 @@ public partial class Player : CharacterBody3D {
 
 	// static variables
 	private static readonly int defaultClipSize = 25;
-	private static readonly int acceleration = 10;
-	private static readonly int decceleration = 15;
-	private static readonly int grappleSpeed = 2000;
+	private static readonly float frictionCoefficient = 0.4f;
+	private static readonly int grappleSpeed = 1000;
 
 	// instance variables
 	public int MaxHealth { get; private set; } = 50000000;
 	public int Health { get; private set; }
-	public float Gravity { get; private set; } = -12f;
+	public float Gravity { get; private set; } = 25;
 	public int Attack { get; private set; } = 100;
-	public float Speed { get; private set; } = 100f;
-	public float JumpSpeed { get; private set; } = 20f;
+	public float Speed { get; private set; } = 200;
+	public float JumpSpeed { get; private set; } = 15;
 	public float MouseSensitivity { get; private set; } = 0.1f;
 	public int ClipSize { get; private set; } = defaultClipSize;
 	public int Bullets { get; private set; } = defaultClipSize;
 	public bool Grappled { get; set; } = false;
 	public bool Reloading { get; private set; } = false;
 	public Vector3 GrapplePoint = new();
-	private Vector3 direction = new();
-	private Vector3 knockbackVector = new();
 	private Node3D RotationalHelper;
-	private LinkedList<Vector3> ForceList = new();
+	private readonly LinkedList<Vector3> forceList = new();
+	private Vector3 movementDirection = new();
 
 	public override void _Ready() {
 		RotationalHelper = GetNode<Node3D>("RotationalHelper");
@@ -60,24 +57,35 @@ public partial class Player : CharacterBody3D {
 	}
 
     public override void _PhysicsProcess(double delta) {
-		if (Health <= 0) return;
+		if (IsOnFloor()) {
+			Velocity *= frictionCoefficient;
+			ApplyForce(movementDirection * Speed / frictionCoefficient);
+		} 
+		if (!GrapplePoint.IsZeroApprox() && Grappled) {
+			Velocity = Position.DirectionTo(GrapplePoint).Normalized() * grappleSpeed / frictionCoefficient * (float) delta;
+		}
 
-		Velocity -= Vector3.Down * Gravity * (float) delta;
-		// Velocity *= 0.5f;
-
-		foreach(Vector3 force in ForceList) {
+		ApplyForce(Vector3.Down * Gravity);
+		foreach(Vector3 force in forceList) {
 			Velocity += force * (float) delta;
 		}
-		ForceList.Clear();
+		forceList.Clear();
 		MoveAndSlide();
 	}
 
 	public override void _Process(double delta) {
+		if (Position.Y < -(GetParent() as GameManager).MapSize.Y) {
+			if (Health <= 0) return;
+			Health = 0;
+			SetPhysicsProcess(false);
+			EmitSignal(SignalName.PlayerDied);
+		}
 		HandleCollision();
 	}
 
-	public void ApplyForce(Vector3 force) {
-		ForceList.AddFirst(force);
+	public void ApplyForce(Vector3 force, bool isInput = false) {
+		if (isInput) movementDirection = force;
+		else forceList.AddFirst(force);
 	}
 
 	public void ResetPlayerState() {
@@ -99,37 +107,8 @@ public partial class Player : CharacterBody3D {
 		
 	}
 
-	// private void ProcessMovement(float delta) {
-	// 	// TODO: REFACTOR MOVEMENT
-	// 	float Ycomponent = delta * Gravity + Velocity.Y;
-
-	// 	if (Grappled && !GrapplePoint.IsZeroApprox()) {
-	// 		// player is grappled
-	// 		Velocity = Position.DirectionTo(GrapplePoint).Normalized() * grappleSpeed * delta;
-
-	// 	} else if (IsOnFloor()) {
-	// 		// player is moving on ground
-	// 		float lerpSpeed;
-	// 		if (direction.Dot(Velocity) > 0) lerpSpeed = acceleration;
-	// 		else lerpSpeed = decceleration;
-			
-	// 		Vector3 velocity = Velocity.Lerp(direction * Speed, lerpSpeed);
-	// 		float Xcomponent = velocity.X * delta + knockbackVector.X;
-	// 		float Zcomponent = velocity.Z * delta + knockbackVector.Z;
-	// 		Velocity = new(Xcomponent, Ycomponent, Zcomponent);
-
-	// 	} else Velocity = new(Velocity.X, Ycomponent, Velocity.Z); // player is midair
-			
-	// 	// handle knockback
-	// 	bool collided = MoveAndSlide();
-	// 	if (collided && GetLastSlideCollision().GetCollider() is not StaticBody3D) {
-	// 		HandleCollision();
-	// 		return;
-	// 	}
-	// 	if (IsOnFloor()) knockbackVector = new();
-	// }
-
 	private void HandleCollision() {
+		// TODO: FIX ENEMY KNOCKBACK
 		if (IFrameTimer.TimeLeft != 0) return;
 		if (GetLastSlideCollision() == null) return;
 
@@ -139,11 +118,13 @@ public partial class Player : CharacterBody3D {
 		IFrameTimer.Start();
 		if (collidingObject is Enemy enemy) {
 			Health -= enemy.Attack;
-			ApplyForce(enemy.Velocity * enemy.KnockbackStrength);
+			Vector3 knockbackVector = new(enemy.Velocity.X, enemy.KnockbackStrength, enemy.Velocity.Z);
+			knockbackVector /= (float) GetProcessDeltaTime();
+			ApplyForce(knockbackVector * enemy.KnockbackStrength);
 
 		} else if (collidingObject is Projectile projectile) {
 			Health -= projectile.Damage;
-			ApplyForce(projectile.LinearVelocity * Projectile.KnockbackStrength);
+			ApplyForce(projectile.LinearVelocity * Projectile.KnockbackStrength * 10);
 		}
 		EmitSignal(SignalName.PlayerDamaged);
 
