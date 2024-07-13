486395fbc926c075fa38cc2b3d26fafa0ebe3318
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-12 19:55:30 -0700

    add applying knockback to player, also add visibility notifier and queue freeing children

--- Scripts/Projectile.cs
+++ Scripts/Projectile.cs
@@ -6,6 +6,7 @@ public partial class Projectile : RigidBody3D {
 	// exported variables
 	[Export] public Timer LifespanTimer;
 	[Export] public AudioStreamPlayer3D PopSFX;
+	[Export] public BubbleVisual Bubble;
 
 	// instance variables
 	public int Damage { get; set; } = 25;
@@ -15,9 +16,12 @@ public partial class Projectile : RigidBody3D {
 		MaxContactsReported = 10;
 		
 		// connect signals
+		VisibleOnScreenNotifier3D visibilityNotifier = GetNode<VisibleOnScreenNotifier3D>("Visibility");
+		visibilityNotifier.Connect("screen_entered", Callable.From(() => Bubble.Visible = true));
+		visibilityNotifier.Connect("screen_exited", Callable.From(() => Bubble.Visible = false));
 		LifespanTimer.Connect("timeout", Callable.From(() => PopBubble()));
 		Connect("body_entered", Callable.From((PhysicsBody3D body) => OnCollision(body)));
-		PopSFX.Connect("finished", Callable.From(() => QueueFree()));
+		PopSFX.Connect("finished", Callable.From(() => DeleteBubble()));
 	}
 
     public void PopBubble() {
@@ -25,9 +29,18 @@ public partial class Projectile : RigidBody3D {
 		PopSFX.Play();
 	}
 
+	private void DeleteBubble() {
+		foreach (var child in GetChildren()) {
+			child.QueueFree();
+		}
+		QueueFree();
+	}
+
 	private void OnCollision(PhysicsBody3D body) {
 		if (body is not CharacterBody3D characterBody3D) return;
-		if (characterBody3D.Name != "Player") return;
+		if (characterBody3D is not Player player) return;
+		player.ApplyForce(LinearVelocity * 0.25f);
+		player.DamagePlayer(Damage);
 		PopBubble();
 	}
 }
