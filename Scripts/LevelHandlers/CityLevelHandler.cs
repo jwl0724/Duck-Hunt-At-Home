68e1cb6e453fec569c45729242ebe8674823f962
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-14 06:10:41 -0700

    adjust shield knockback formula

--- Scripts/LevelHandlers/CityLevelHandler.cs
+++ Scripts/LevelHandlers/CityLevelHandler.cs
@@ -23,8 +23,13 @@ public partial class CityLevelHandler : Node {
 			player.Position = player.Position.Lerp(Vector3.Zero, 0.1f);
 			return;
 		}
-		Vector3 force = new(-player.Velocity.X, 4, -player.Velocity.Z);
-		force = force * 2 / (float) GetProcessDeltaTime();
+		Vector3 force = player.Position.DirectionTo(Vector3.Zero) * CalculateKnockback(player.Velocity);
 		player.ApplyForce(force);
+	}
+
+	private float CalculateKnockback(Vector3 velocity) {
+		float highestAxis = Math.Max(Math.Abs(velocity.X), Math.Max(Math.Abs(velocity.Y), Math.Abs(velocity.Z)));
+		if (highestAxis <= 30) return 30;
+		else return highestAxis;
 	}
 }
