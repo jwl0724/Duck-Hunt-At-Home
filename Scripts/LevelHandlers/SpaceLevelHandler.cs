ca9250ce22fcac1c56b62411d169bc47ba17abad
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-14 09:03:02 -0700

    create level handler for space level

---
+++ Scripts/LevelHandlers/SpaceLevelHandler.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class SpaceLevelHandler : Node3D {
+	[Export] Player Player;
+	
+	public override void _Ready() {
+		Player.Gravity = 2;
+		Player.JumpSpeed = 4;
+		Player.GrappleSpeed = 300;
+	}
+
+	public override void _Process(double delta) {
+		
+	}
+}
