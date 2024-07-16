dd0a894a04a1b22a17e97260f2fc0f617c59a4a5
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-16 07:36:45 -0700

    move splash screen to separate scene

--- Scripts/ScreenHandlers/MenuHandler.cs
+++ Scripts/ScreenHandlers/MenuHandler.cs
@@ -1,20 +1,28 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class MenuHandler : Control {
-	public enum MenuScreen { BootUp, MainMenu, LevelSelect, InGame }
+	public enum MenuScreen { MainMenu, LevelSelect, InGame }
 	
-	[Export] Control BootUpScreen;
 	[Export] Control MainMenuScreen;
 	[Export] Control LevelSelectScreen;
 
-	public MenuScreen CurrentScreen { get; private set; } = MenuScreen.BootUp;
+	[Signal] public delegate void ScreenChangeEventHandler();
+
+	public MenuScreen CurrentScreen { get; private set; } = MenuScreen.MainMenu;
 
 	public override void _Ready() {
 		MainMenuScreen.Connect("LeaveMainMenu", Callable.From(() => TransitionScreen(MenuScreen.LevelSelect)));
-		LevelSelectScreen.Connect("SelectedLevel", Callable.From(() => TransitionScreen(MenuScreen.InGame)));
+		LevelSelectScreen.Connect("SelectedLevel", Callable.From((int levelIndex) => StartGame(levelIndex)));
 		LevelSelectScreen.Connect("BackToMainMenu", Callable.From(() => TransitionScreen(MenuScreen.MainMenu)));
-		RunBootUp();
+		TransitionScreen(MenuScreen.MainMenu);
+	}
+
+	private void StartGame(int levelIndex) {
+		if (levelIndex == 0) GetTree().ChangeSceneToFile("res://Scenes/Screens/CityLevel.tscn");
+		else if (levelIndex == 1) GetTree().ChangeSceneToFile("res://Scenes/Screens/PlainsLevel.tscn");
+		else if (levelIndex == 2) GetTree().ChangeSceneToFile("res://Scenes/Screens/SpaceLevel.tscn");
 	}
 
 	private void TransitionScreen(MenuScreen screen) {
@@ -23,12 +31,5 @@ public partial class MenuHandler : Control {
 			if (menu.Name == Enum.GetName(CurrentScreen)) menu.Visible = true;
 			else menu.Visible = false;
 		}
-	}
-
-	private void RunBootUp() {
-		TransitionScreen(MenuScreen.BootUp);
-		AnimationPlayer animator = BootUpScreen.GetNode<AnimationPlayer>("Animation");
-		animator.Connect("animation_finished", Callable.From((StringName name) => TransitionScreen(MenuScreen.MainMenu)));
-		animator.Play("LogoIntro");
 	}
 }
