b7620781621ef819d1fe53cc97caa733b2492117
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-07-16 22:41:17 -0700

    add click sound effect to buttons

--- Scripts/ScreenHandlers/LevelSelectHandler.cs
+++ Scripts/ScreenHandlers/LevelSelectHandler.cs
@@ -22,13 +22,22 @@ public partial class LevelSelectHandler : Control {
 		GetNode<Button>("LeftArrow").Connect("button_up", Callable.From(() => CycleLevel(-1)));
 		GetNode<Button>("RightArrow").Connect("button_up", Callable.From(() => CycleLevel(1)));
 		GetNode<Button>("PlayLevelButton").Connect("button_up", Callable.From(() => OnSelectedLevel()));
-		GetNode<Button>("BackButton").Connect("button_up", Callable.From(() => EmitSignal(SignalName.BackToMainMenu)));
+		GetNode<Button>("BackButton").Connect("button_up", Callable.From(() => OnBack()));
 		GetNode<Control>("LoadingScreen").Connect("draw", Callable.From(() => OnLoadingScreenDraw()));
 
-		CycleLevel(0); // load an image on first load up
+		// load an image on first load up
+		GetNode<LevelPreviewHandler>("LevelPreview").DisplayLevel(
+			Enum.GetName(typeof(Level), levelIndex), levelNames[levelIndex], levelDifficulties[levelIndex]
+		);
+	}
+
+	private void OnBack() {
+		(GetParent() as MenuHandler).SoundCollection.Play("Click", overlapMusic: true);
+		EmitSignal(SignalName.BackToMainMenu);
 	}
 
 	private void OnSelectedLevel() {
+		(GetParent() as MenuHandler).SoundCollection.Play("Click", overlapMusic: true);
 		Control loadingScreen = GetNode<Control>("LoadingScreen");
 		TextureRect loadImage = loadingScreen.GetNode<TextureRect>("LevelImage");
 		
@@ -49,7 +58,8 @@ public partial class LevelSelectHandler : Control {
 		levelIndex += indexShift;
 		if (levelIndex < 0) levelIndex = totalLevels - 1;
 		else if (levelIndex >= totalLevels) levelIndex = 0;
-
+		
+		(GetParent() as MenuHandler).SoundCollection.Play("Click", overlapMusic: true);
 		GetNode<LevelPreviewHandler>("LevelPreview").DisplayLevel(
 			Enum.GetName(typeof(Level), levelIndex), levelNames[levelIndex], levelDifficulties[levelIndex]
 		);
